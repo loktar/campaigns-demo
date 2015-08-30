077d445570354a4732fcf1a53e0550294119ec6e
Ian Fisher <loktar@gmail.com>
2015-08-29 17:23:04 -0700

    Added /api/v1/campaings.json route with simple reverse proxy-ish behavior

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,18 @@
-require 'sinatra'
 require 'haml'
+require 'json'
+require 'open-uri'
+require 'sinatra'
 
 get '/' do
   haml :index
+end
+
+get '/api/v1/campaigns.json' do
+  content_type :json
+
+  # For a simple app, blocking on the indiegogo api call is fine, but eventually we should handle
+  # when the indiegogo api is slow/unresponsive. Leaving it like this risks our server's workers
+  # being blocked in that scenario and could negatively affect the rest of our app.
+  uri = 'https://api.indiegogo.com/1/campaigns.json?api_token=e377270bf1e9121da34cb6dff0e8af52a03296766a8e955c19f62f593651b346'
+  URI.parse(uri).read
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -12,4 +12,26 @@ describe 'App' do
       expect(last_response).to be_ok
     end
   end
+
+  describe 'the API' do
+    describe 'the campaigns endpoint' do
+      before do
+        # TODO: Use cached response to speed up specs
+        get '/api/v1/campaigns.json'
+      end
+
+      it 'should be a success' do
+        expect(last_response).to be_ok
+      end
+
+      it 'should be valid json' do
+        JSON.parse(last_response.body)
+      end
+
+      it 'should contain campaign data' do
+        json = JSON.parse(last_response.body)
+        expect(json['response'][0]['funding_type']).to be_a String
+      end
+    end
+  end
 end
